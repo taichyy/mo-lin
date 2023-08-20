4a168fc14cea7a17cc1479cd7f010d3d604c503f
taichyy <chinzeilulu@gmail.com>
2023-08-21 02:46:25 +0800

    Try fix Vercel deployment error

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Let's Keep In Touch</h1>
+      <h1 className={styles.title}>Let us Keep In Touch</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
           <Image
